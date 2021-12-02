0f34b9af658213748557c2b88db380eae54cf965
Reian Asiñero <reianasinero@gmail.com>
2021-12-02 12:44:58 +0800

    intro

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css'
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1 className="title-text">Reian Asiñero</h1>
+            <hr className="horizontal-line"></hr>
+            <h3 className="subtitle-text">Front-Ent Web Developer | Photographer | Graphic Designer</h3>
+        </div>
+    )
+}
+
+export default Title
