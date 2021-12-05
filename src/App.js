924723e05ae20e00c4fdbba90720eb83099a9ba3
Reian Asiñero <reianasinero@gmail.com>
2021-12-05 15:51:24 +0800

    new placement of Title and SubTitle and added Profile Picture and about me details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Title from './Title.js';
 import Header from './Header.js';
 import React from 'react';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Title />
+      <div className="Container">
+        <Header />
+        <Title />
+      </div>
     </div>
   )
 }
--- src/Title.js
+++ src/Title.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './Title.css'
+import './Title.css';
+import logo from './reian.png';
 
 function Title() {
     return (
         <div className="Title">
             <h1 className="title-text">Reian Asiñero</h1>
             <h3 className="subtitle-text">Front-End Web Developer | Photographer | Graphic Designer</h3>
+            <img className="my-portrait" src={logo} alt="My Portrait" />;
+            <p className="paragraph-aboutMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Feugiat pretium nibh ipsum consequat. Varius morbi enim nunc faucibus a pellentesque sit amet porttitor.</p>
         </div>
     )
 }
